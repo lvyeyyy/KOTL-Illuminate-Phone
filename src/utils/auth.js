a2b04447de8105c7eba9d0dd4246fa73e1add67f
lvyeyyy <2845325306@qq.com>
2022-10-27 09:11:22 +0800

    edit

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,6 @@
 import Cookies from 'js-cookie'
 
-const TokenKey = 'vue_admin_template_phone_token'
+const TokenKey = 'vue_admin_template_token'
 
 export function getToken() {
   return Cookies.get(TokenKey)
